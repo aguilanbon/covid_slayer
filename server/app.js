a0559ab7949ec54b4d20256741b931c67ef57672
Bon Aguilan <aguilanbon@gmail.com>
2025-07-31 01:42:44 +0800

    init db connection and craete, get user

--- server/app.js
+++ server/app.js
@@ -1,15 +1,19 @@
 import express from "express";
 import "dotenv/config";
-
+import connectDB from "./config/db.js";
 import userRouter from "./routes/user.routes.js";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api", userRouter);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+connectDB().then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+  });
 });
 
 export default app;
--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,11 +1,25 @@
 import express from "express";
+import User from "../models/User.js";
 
-export const createUser = (req, res) => {
-  res.status(201).send({ message: "User created successfully" });
+export const createUser = async (req, res) => {
+  const { fullName, email, password, avatar } = req.body;
+  try {
+    const user = await User.create({ fullName, email, password, avatar });
+    res.status(201).send({ message: "User created successfully", user });
+  } catch (error) {
+    console.error("Error creating user:", error);
+    res.status(500).send({ message: "Error creating user" });
+  }
 };
 
-export const getUsers = (req, res) => {
-  res.status(200).send({ message: "Users retrieved successfully" });
+export const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).send({ message: "Users retrieved successfully", users });
+  } catch (error) {
+    console.error("Error retrieving users:", error);
+    res.status(500).send({ message: "Error retrieving users" });
+  }
 };
 
 export default { createUser, getUsers };
